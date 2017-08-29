3a4bf9667aec9ca37f367c27130cc0a8035ed02e
DiyLecko <diy_lecko@naver.com>
2017-08-29 15:22:05 +0900

    1.0.9

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const cheerio = require("cheerio");
 
 const npmurl = 'https://www.npmjs.com/package/';
 
-fs.readFile('package.json', (err, data) => {
-	var packages = JSON.parse(data.toString());
-	var dependencies = packages.dependencies;
-
-	for (var dependency in dependencies) {
-		var url = npmurl + dependency;
-		(function(url) { request(url, function(error, response, body) {  
-  	  if (error) throw error;
-
-      var $ = cheerio.load(body);
-      var license = $('.sidebar').children('ul.box').first().children('li').last().children('a').text();
-
-      console.log(url);
-      console.log('License: ', license);
-	  })}(url));
-	}
-});
-
 exports.export = function() {
   fs.readFile('package.json', (err, data) => {
     var packages = JSON.parse(data.toString());
