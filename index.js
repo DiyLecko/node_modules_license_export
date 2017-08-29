dda0afeae40d9115bc70e55ad728bf6007e7614c
DiyLecko <diy_lecko@naver.com>
2017-08-29 15:17:15 +0900

    1.0.7

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ fs.readFile('package.json', (err, data) => {
 	  })}(url));
 	}
 });
+
+exports.export = function() {
+  fs.readFile('package.json', (err, data) => {
+    var packages = JSON.parse(data.toString());
+    var dependencies = packages.dependencies;
+
+    for (var dependency in dependencies) {
+      var url = npmurl + dependency;
+      (function(url) { request(url, function(error, response, body) {  
+        if (error) throw error;
+
+        var $ = cheerio.load(body);
+        var license = $('.sidebar').children('ul.box').first().children('li').last().children('a').text();
+
+        console.log(url);
+        console.log('License: ', license);
+      })}(url));
+    }
+  });
+};
